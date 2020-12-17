f33eaaebfd46640a7c449459e958ec0da05e90d1
hmugera <mugerahughes@gmail.com>
2020-12-18 01:34:22 +0300

    working on ui,and added songs data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import './styles/app.scss'
+import data from './songs-data/data'
 
-
+//Components
+import Player from './components/player';
+import Song from "./components/song"
 function App() {
-  return( <div>hughes</div>)
+  return( <div>
+    <Song/>
+    <Player/>
+  </div>)
 }
 
 export default App;
