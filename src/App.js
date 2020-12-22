09725ae28f804ca6656b0687e77b852eccadb458
hmugera <mugerahughes@gmail.com>
2020-12-22 22:21:02 +0300

    setting up code formatter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import './styles/app.scss'
-import data from './songs-data/data'
+import React, { useState } from "react";
+import "./styles/app.scss";
+
+//import data
+import data from "./songs-data/data";
 
 //Components
-import Player from './components/player';
-import Song from "./components/song"
+import Player from "./components/player";
+import Song from "./components/song";
 function App() {
-  return( <div>
-    <Song/>
-    <Player/>
-  </div>)
+	//state
+	const [songs, setSongs] = useState(data());
+	return (
+		<div>
+			<Song />
+			<Player />
+			{console.log(songs)}
+		</div>
+	);
 }
 
 export default App;
